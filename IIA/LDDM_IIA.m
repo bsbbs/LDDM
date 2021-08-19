1f711ec3ad76767ac23ff69695ddec71362e1baf
BoShen-NeuroSci <boshen89@gmail.com>
2021-08-19 17:02:17 -0400

    initial commit

--- IIA/LDDM_IIA.m
+++ IIA/LDDM_IIA.m
@@ -14,21 +14,29 @@ fontsize = 14;
 mksz = 5;
 lwd = 2;
 %% initalize parameters as in the paper of Wong & Wang, 2006
+task = 'RT';%'FD';
 a = eye(3)*15;
 b = eye(3)*1.02;
 w = ones(3);
-sgm = 10;
 scale = 256;
 tauR = .1; % second
 tauG = .1; % second
 tauI = .1; % second
 Tau = [tauR, tauG, tauI];
 % simulation parameters
-dur = 5; % second
 dt = .001; % second
 presentt = dt;
-triggert = presentt;
-stimdur = dur;
+if strcmp(task,'FD')
+    sgm = 5;
+    dur = 7; % second
+    triggert = 2.5; %presentt;
+    stimdur = 2.5;
+elseif strcmp(task,'RT')
+    sgm = 20;
+    dur = 5; % second
+    triggert = presentt;
+    stimdur = dur;
+end
 thresh = 70; % Hz
 initialvals = [4, 4, 4;12, 12, 12; 0, 0, 0]; % for R, G, and I variables
 stoprule = 1;
@@ -44,6 +52,9 @@ for c3 = [.2 1]
 end
 ylim([0,80]);
 %% plot psychometric function
+h = figure;
+figname = sprintf('IIA_LDDM_%s',task);
+aspect = [7, 6];
 % setting input matrix
 c = [.032, .064, .128, .256, .512]';
 c1 = [1 - flip(c); 1; 1 + c];
@@ -51,11 +62,13 @@ c2 = ones(size(c1));
 c3 = 0:.5:2;
 [cp.cp1, ~] = meshgrid(c1, c3);
 [cp.cp2, cp.cp3] = meshgrid(c2, c3);
+clear Vinput;
 Vinput.V1 = cp.cp1*scale;
 Vinput.V2 = cp.cp2*scale;
 Vinput.V3 = cp.cp3*scale;
 % simulation
-simrslt = fullfile(outdir,sprintf('LDDM_%ic1_%ic2_%ic3_sim%i.mat',length(c1),length(unique(c2)),length(unique(c3)),sims));
+filename = sprintf('LDDM_%s_%ic1_%ic2_%ic3_sgm%2.1f_sim%i',task,length(c1),length(unique(c2)),length(unique(c3)),sgm,sims);
+simrslt = fullfile(outdir,[filename, '.mat']);
 if ~exist(simrslt,'file')
     [choice, rt] = LDDM_GPU3(Vinput, w, a, b, sgm, Tau, dur,...
         dt, presentt, triggert, thresh, initialvals, stimdur, stoprule, sims);
@@ -63,13 +76,10 @@ if ~exist(simrslt,'file')
 else
     load(simrslt);
 end
-% plot
+% plot psychometric function
 cndratio = sum(choice == 1, 3)./(sum(choice == 1, 3) + sum(choice == 2, 3)); % conditional ratio of choosing 1 vs. choosing 2
 x = cp.cp1 - cp.cp2;
-h = figure;
-filename = 'IIA_LDDM';
-aspect = [7, 3];
-subplot(1,2,1);
+subplot(2,2,1);
 hold on;
 mycol = colormap(jet(41));
 for i = 1:size(x,1)
@@ -79,19 +89,41 @@ xlabel('V_1 - V_2');
 ylabel({'Conditional choice probability';'Opt. 1 vs. Opt. 2'});
 lgd = legend(string(num2cell(c3)), 'FontSize', fontsize - 6, 'Location','best', 'box','off');
 title(lgd, 'V_3');
-savefig(h, filename, outdir, fontsize, aspect);
-%% plot conditional ratio as a function of V3
-c = [.064]';
+savefig(h, figname, outdir, fontsize, aspect);
+% plot RT as a functon of V1 - V2
+subplot(2,2,3);
+hold on;
+mycol = colormap(jet(41));
+rtmean = mean(rt,3,'omitnan');
+for i = 1:size(x,1)
+    plot(x(i,:),rtmean(i,:),'.-','color',mycol(1+(i-1)*10,:),'LineWidth',lwd-1,'MarkerSize',mksz*3);
+end
+xlabel('V_1 - V_2');
+ylabel({'RT (s)'});
+savefig(h, figname, outdir, fontsize, aspect);
+% plot simulation settings
+subplot(2,2,4); hold on;
+y = 3*ones(size(c2));
+plot(c2, y,'.k','MarkerSize',mksz*3);
+y = 3.1*ones(size(c1));
+plot(c1, y, '.k','MarkerSize',mksz*3);
+% plot conditional ratio as a function of V3
+if strcmp(task,'FD')
+    c = [.512]';
+elseif strcmp(task,'RT')
+    c = [.256]';
+end
 c1 = [1 + c];
 c2 = ones(size(c1));
-c3 = 0:.05:2;
+c3 = 0:.04:2;
 [cp.cp1, ~] = meshgrid(c1, c3);
 [cp.cp2, cp.cp3] = meshgrid(c2, c3);
 Vinput.V1 = cp.cp1*scale;
 Vinput.V2 = cp.cp2*scale;
 Vinput.V3 = cp.cp3*scale;
 % simulation
-simrslt = fullfile(outdir,sprintf('LDDM_%ic1_%ic2_%ic3_sim%i.mat',length(c1),length(unique(c2)),length(unique(c3)),sims));
+filename = sprintf('LDDM_%s_%ic1_%ic2_%ic3_sgm%2.1f_sim%i',task,length(c1),length(unique(c2)),length(unique(c3)),sgm,sims);
+simrslt = fullfile(outdir,[filename, '.mat']);
 if ~exist(simrslt,'file')
     [choice, rt] = LDDM_GPU3(Vinput, w, a, b, sgm, Tau, dur,...
         dt, presentt, triggert, thresh, initialvals, stimdur, stoprule, sims);
@@ -101,12 +133,28 @@ else
 end
 % plot
 cndratio = sum(choice == 1, 3)./(sum(choice == 1, 3) + sum(choice == 2, 3)); % conditional ratio of choosing 1 vs. choosing 2
-subplot(1,2,2); hold on;
+subplot(2,2,2); hold on;
 mycol = colormap(jet(length(c3)));
 for i = 1:length(c3)
     plot(cp.cp3(i,1), cndratio(i,1),'.','color',mycol(i,:),'LineWidth',lwd,'MarkerSize',mksz*3);
 end
-ylim([.5,.9]);
+if strcmp(task,'FD')
+    ylim([.8,.9]);  
+elseif strcmp(task,'RT')
+    ylim([.75,.95]);
+end
 xlabel('V_3');
 ylabel({'Conditional choice probability';'Opt. 1 vs. Opt. 2'});
-savefig(h, filename, outdir, fontsize, aspect);
+savefig(h, figname, outdir, fontsize, aspect);
+% plot simulation settings
+subplot(2,2,4); hold on;
+y = 2.9*ones(size(c3));
+for i = 1:length(c3)
+    plot(c3(i),y(i),'.','color',mycol(i,:),'MarkerSize',mksz*3);
+end
+ylim([2.8,3.5]);
+yticks([2.9,3.0,3.1]);
+yticklabels({'V_3','V_2','V_1'});
+set(get(gca,'YAxis'),'Visible','on');
+xlabel('Option values');
+savefig(h, figname, outdir, fontsize, aspect);
--- IIA/WW06_IIA.m
+++ IIA/WW06_IIA.m
@@ -33,6 +33,9 @@ initialvals = [2, 2, 2; .1, .1, .1]; % for H and S variables
 stoprule = 1;
 sims = 10240*2; % number of iterations
 %% plot psychometric function
+h = figure;
+figname = 'IIA_WW06';
+aspect = [7, 6];
 % setting input matrix
 c = [.032, .064, .128, .256, .512]';
 c1 = [1 - flip(c); 1; 1 + c];
@@ -52,10 +55,7 @@ end
 % plot
 cndratio = sum(choice == 1, 3)./(sum(choice == 1, 3) + sum(choice == 2, 3)); % conditional ratio of choosing 1 vs. choosing 2
 x = cp.cp1 - cp.cp2;
-h = figure;
-filename = 'IIA_WW06';
-aspect = [7, 3];
-subplot(1,2,1);
+subplot(2,2,1);
 hold on;
 mycol = colormap(jet(41));
 for i = 1:size(x,1)
@@ -65,8 +65,25 @@ xlabel('V_1 - V_2');
 ylabel({'Conditional choice probability';'Opt. 1 vs. Opt. 2'});
 lgd = legend(string(num2cell(c3)), 'FontSize', fontsize - 6, 'Location','best', 'box','off');
 title(lgd, 'V_3');
-savefig(h, filename, outdir, fontsize, aspect);
-%% plot conditional ratio as a function of V3
+savefig(h, figname, outdir, fontsize, aspect);
+% plot RT as a functon of V1 - V2
+subplot(2,2,3);
+hold on;
+mycol = colormap(jet(41));
+rtmean = mean(rt,3,'omitnan');
+for i = 1:size(x,1)
+    plot(x(i,:),rtmean(i,:),'.-','color',mycol(1+(i-1)*10,:),'LineWidth',lwd-1,'MarkerSize',mksz*3);
+end
+xlabel('V_1 - V_2');
+ylabel({'RT (s)'});
+savefig(h, figname, outdir, fontsize, aspect);
+% plot simulation settings
+subplot(2,2,4); hold on;
+y = 3*ones(size(c2));
+plot(c2, y,'.k','MarkerSize',mksz*3);
+y = 3.1*ones(size(c1));
+plot(c1, y, '.k','MarkerSize',mksz*3);
+% plot conditional ratio as a function of V3
 c = [.064]';
 c1 = [1 + c];
 c2 = ones(size(c1));
@@ -84,7 +101,7 @@ else
 end
 % plot
 cndratio = sum(choice == 1, 3)./(sum(choice == 1, 3) + sum(choice == 2, 3)); % conditional ratio of choosing 1 vs. choosing 2
-subplot(1,2,2); hold on;
+subplot(2,2,2); hold on;
 mycol = colormap(jet(length(c3)));
 for i = 1:length(c3)
     plot(cp.cp3(i,1), cndratio(i,1),'.','color',mycol(i,:),'LineWidth',lwd,'MarkerSize',mksz*3);
@@ -92,4 +109,16 @@ end
 ylim([.5,.9]);
 xlabel('V_3');
 ylabel({'Conditional choice probability';'Opt. 1 vs. Opt. 2'});
-savefig(h, filename, outdir, fontsize, aspect);
+savefig(h, figname, outdir, fontsize, aspect);
+% plot simulation settings
+subplot(2,2,4); hold on;
+y = 2.9*ones(size(c3));
+for i = 1:length(c3)
+    plot(c3(i),y(i),'.','color',mycol(i,:),'MarkerSize',mksz*3);
+end
+ylim([2.8,3.5]);
+yticks([2.9,3.0,3.1]);
+yticklabels({'V_3','V_2','V_1'});
+set(get(gca,'YAxis'),'Visible','on');
+xlabel('Option values');
+savefig(h, figname, outdir, fontsize, aspect);
