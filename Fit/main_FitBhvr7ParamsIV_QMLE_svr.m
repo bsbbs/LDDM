9f6a78e9e4c96b06cddf613ac7340d3b015fb70c
Bo Shen <40451542+bsbbs@users.noreply.github.com>
2022-02-07 23:16:21 -0500

    plot

--- Fit/main_FitBhvr7ParamsIV_QMLE_svr.m
+++ Fit/main_FitBhvr7ParamsIV_QMLE_svr.m
@@ -93,11 +93,12 @@ if ~exist(plot_dir,'dir')
 end
 dataDynmc = load('./Data/Data.mat');
 dataBhvr = LoadRoitmanData('../RoitmanDataCode');
-randseed = 69094639;
+randseed = 24356545;
 rng(randseed);
 % a, b, noise, scale, tauRGI, nLL
+% params = [0.000056	1.433901	24.837397	3254.833078	0.183152	0.248698	0.309921	16542.77267];
 params = [0	1.433631	25.35945	3251.289056	0.185325	0.224459	0.323132	16539.138186];
-name = sprintf('a%2.2f_b%1.2f_sgm%2.1f_scale%4.1f_tau%1.2f_%1.2f_%1.2f_sim1024',params(1:7));
+name = sprintf('a%2.2f_b%1.2f_sgm%2.1f_scale%4.1f_tau%1.2f_%1.2f_%1.2f',params(1:7));
 if ~exist(fullfile(plot_dir,sprintf('PlotData_%s.mat',name)),'file')
     tic;
     [nLL, Chi2, BIC, AIC, rtmat, choicemat] = LDDMFitBhvr7ParamsIV_QMLE_GPU(params, dataBhvr);
@@ -112,8 +113,11 @@ end
 lwd = 1;
 mksz = 3;
 fontsize = 11;
+randseed = 24356545;
+rng(randseed);
 % a, b, noise, scale, tauRGI, nLL
-params = [	0	1.433631	25.35945	3251.289056	0.185325	0.224459	0.323132	16539.138186];
+% params = [0.000056	1.433901	24.837397	3254.833078	0.183152	0.248698	0.309921	16542.77267];
+params = [0	1.433631	25.35945	3251.289056	0.185325	0.224459	0.323132	16539.138186];
 simname = sprintf('LDDM_Dynmc_a%2.2f_b%1.2f_sgm%2.1f_scale%4.1f_tau%1.2f_%1.2f_%1.2f_nLL%4.0f',params);
 
 a = params(1)*eye(2);
@@ -158,7 +162,7 @@ for vi = 2:6
 %     [R, G, I, rt, choice] = LcDsInhbt(Vinput, w, a, b, sgm, Tau, dur,...
 %         dt, presentt, triggert, thresh, initialvals, stimdur, stoprule);
     [~, ~, R, G, I] = LDDM(Vprior, Vinput, w, a, b, sgm, Tau, predur, dur,...
-    dt, presentt, triggert, 1.5*thresh, initialvals, stimdur, stoprule);
+    dt, presentt, triggert, thresh, initialvals, stimdur, stoprule);
     lgd2(vi-1) = plot(R(:,2), 'k-.', 'Color', mygray(vi,:), 'LineWidth',lwd);
     lgd1(vi-1) = plot(R(R(:,1)<=thresh,1), 'k-', 'Color', mygray(vi,:), 'LineWidth',lwd);
 end
