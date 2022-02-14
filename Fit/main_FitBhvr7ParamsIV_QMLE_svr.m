e5fba7959916e58e834bd6195d610a5f604ceaf6
bsbbs <boshen89@gmail.com>
2022-02-14 11:40:29 +0800

    RT plot

--- Fit/main_FitBhvr7ParamsIV_QMLE_svr.m
+++ Fit/main_FitBhvr7ParamsIV_QMLE_svr.m
@@ -304,6 +304,38 @@ h.PaperUnits = 'inches';
 h.PaperPosition = [0 0 3.0 10];
 %saveas(h,fullfile(plot_dir,sprintf('RTDistrb_%s.fig',name)),'fig');
 saveas(h,fullfile(plot_dir,sprintf('RTDistrb_%s.eps',name)),'epsc2');
+%% panel a, ditribution of RT and fitted line
+colorpalette = {'#ef476f','#ffd166','#06d6a0','#118ab2','#073b4c'};
+aspect8 = [2, 6.4]; % for the long format RT distribution fitting panels
+h = figure;
+filename = 'Fig5a';
+for ii = 1:6
+    subplot(6,1,ii);hold on;
+    bar(dataBhvr.bincenter(ii,1:30),dataBhvr.histmat(ii,1:30)*1024,'FaceColor',colorpalette{3},'EdgeAlpha',0);
+    bar(dataBhvr.bincenter(ii,1:30),-dataBhvr.histmat(ii,31:60)*1024,'FaceColor',colorpalette{2},'EdgeAlpha',0,'EdgeColor','none');
+    plot(BinMiddle{ii},bank1{ii},'Color',colorpalette{4},'LineWidth',lwd);
+    plot(BinMiddle{ii},-bank2{ii},'Color',colorpalette{1},'LineWidth',lwd);
+    if ii == 7
+        legend({'','','Correct','Error'},'NumColumns',2,'Location','North');
+        legend('boxoff');
+    end
+    ylim([-60,100]);
+    yticks([-50:50:100]);
+    yticklabels({'50','0','50','100'});
+    xlim([100 1762]/1000);
+    xticks([.5,1.0,1.5]);
+    if ii == 6
+        xticklabels({'.5','1.0','1.5'});
+        xlabel('Reaction time (s)');
+    else
+        xticklabels({});
+    end
+    if ii == 1
+        ylabel(' ');
+    end
+    set(gca, 'box','off');
+    savefigs(h, filename, plot_dir, fontsize, aspect8);
+end
 %% aggregated RT & ACC
 lwd = 1;
 mksz = 3;
