709678ed02f30d16e5c11b496302e14454412da3
Bo Shen <40451542+bsbbs@users.noreply.github.com>
2022-03-07 21:39:18 -0500

    Fit Louie 2011

--- Fit/FitLouie2011_R0GAMMA.m
+++ Fit/FitLouie2011_R0GAMMA.m
@@ -1,7 +1,9 @@
 % Fit the mean firing rates from Louie, et al., 2011
-cd('/Volumes/GoogleDrive/My Drive/LDDM/Fit');
+cd('G:\My Drive\LDDM\Fit');
+% cd('/Volumes/GoogleDrive/My Drive/LDDM/Fit');
 addpath(genpath('./bads-master'));
-out_dir = './Rslts/FitLouie2011';
+addpath(genpath('C:\Users\Bo\Documents\LDDM\utils'));
+out_dir = './Rslts/FitLouie2011/R0GAMMA';
 if ~exist(out_dir,'dir')
     mkdir(out_dir);
 end
@@ -21,12 +23,13 @@ Vilist = unique(Vi);
 %% Values in equilibirum
 a = 50;
 GAMMA = 1;
-I0 = 0.1;
-x = [a,GAMMA,I0];
-LB = [0, 1, 0.1];
-UB = [80, 500, 500];
-PLB = [5, 100, 80];
-PUB = [40, 300, 120];
+R0 = 10;
+S = .05;
+x = [a,GAMMA,R0,S];
+LB = [0, 1, 0.1, .6];
+UB = [80, 500, 500, 1.7];
+PLB = [5, 100, 80, .9];
+PUB = [40, 300, 120, 1.1];
 tic;
 [RSS, R1s] = OLS(x,Vi,Vo,FR);
 toc
@@ -35,6 +38,7 @@ f = @(x)OLS(x, Vi, Vo, FR);
 % opt = optimset('Display', 'on', 'MaxFunEvals', 1000, 'MaxIter', 1000, 'TolFun', 1e-3, 'Algorithm', 'active-set');
 count = 0;
 summaries = [];
+reports = [];
 visulize = 1;
 options = bads('defaults');     % Default options
 options.Display = 'iter';
@@ -43,14 +47,15 @@ options.UncertaintyHandling = false;    % Function is deterministic
 for i = 1:20
     fprintf('Fit # %i: ',i);
     x0 = randomX(LB,UB);
-    [x,fval,~,output] = bads(f,x0,LB,UB,PLB,PUB,options);
+    [x,fval,~,report] = bads(f,x0,LB,UB,PLB,PUB,options);
     
     % [X, tMin] = fmincon(f, X0, [],[],[],[], lowerBd, upperBd, [], opt);
     summaries(end+1,:)  = [x fval];
-    %%
+    reports{i} = report;
+    %
     if visulize
         h = figure(i);  hold on;
-        filename = sprintf('FitLouie_bads_%i_a%1.2f_GAMMA%3.1f_I0%3.1f_fval%1.3f',i,x,fval);
+        filename = sprintf('FitLouie_bads_%i_a%1.2f_GAMMA%3.1f_I0%3.1f_S%1.2f_fval%1.3f',i,x,fval);
         [RSS, R1s] = OLS(x,Vi,Vo,FR);
         for ii = 1:numel(Vilist)
             mask = Vi == Vilist(ii);
@@ -63,16 +68,17 @@ for i = 1:20
         xlabel('V_{out}');
         savefigs(h, filename, plot_dir, 12, [2.5, 2.5]);
     end
-    %%
+    %
     fprintf('\n');
     pause(.3);
-    if ~isnan(tMin) && (count == 0 || (fval < ll_min && sum(x ~= xBest)>0))
+    if ~isnan(fval) && (count == 0 || (fval < RSS_min && sum(x ~= xBest)>0))
         xBest = x;
-        ll_min = fval;
+        RSS_min = fval;
         count = count + 1;
         %              sprintf(['Subject %d\t: Round-%d ' repmat('%8.3f\t', 1, length(XBest)) '\n'], jj, i, XBest)
     end
 end
+save(fullfile(out_dir,'FitRslt.mat'),'summaries','xBest','RSS_min','reports');
 
 %%
 
@@ -80,37 +86,38 @@ end
 function [RSS, R1s] = OLS(x,Vi,Vo,FR)
 a = x(1);
 GAMMA = x(2);
-I0 = x(3);
+R0 = x(3);
+S = x(4);
 if numel(Vi) ~= numel(Vo)
     error('Input Vi dimension does not match Vo');
 end
 R1s = [];
 for i = 1:numel(Vi)
     % fprintf('%i.',i)
-    V = [Vi(i) Vo(i)]+I0;
-    tmp = SolveEqlb(a,GAMMA,V);
+    V = [Vi(i) Vo(i)];
+    tmp = SolveEqlb(a,GAMMA,R0,V);
     if numel(tmp) > 1
         warning('Real positive solution more than 1. Recorded only the first value.');
     end
     R1s(i,:) = tmp(1);
 end
 % fprintf('\n');
-R1s = max(FR)*R1s/max(R1s);
+R1s = S*R1s/max(R1s)*max(FR);
 RSS = sum((R1s - FR).^2);
 end
 %%
-function R1 = SolveEqlb(a,GAMMA,V)
+function R1 = SolveEqlb(a,GAMMA,R0,V)
 w = 1;
 v = 1;
 b = 0;
 syms R1 R2
 if V(1) > 0 && V(2) > 0
-    eqns = [(V(1)/R1 - (w - b)*R1 - (GAMMA-a))/v == R2, ... % dR1/dt = 0
-        (V(2)/R2 - (w - b)*R2 - (GAMMA-a))/v == R1];% dR2/dt = 0
+    eqns = [(V(1)/(R1-R0) - (w - b)*R1 - (GAMMA-a*R1/(R1-R0)))/v == R2, ... % dR1/dt = 0
+        (V(2)/(R2-R0) - (w - b)*R2 - (GAMMA-a*R2/(R2-R0)))/v == R1];% dR2/dt = 0
     vars = [R1 R2];
     [AnswR1, ~] = solve(eqns, vars);
 elseif V(1) > 0 && V(2) <= 0
-    eqns = [(V(1)/R1 - (w - b)*R1 - (GAMMA-a))/v == 0]; % dR1/dt = 0
+    eqns = [(V(1)/(R1-R0) - (w - b)*R1 - (GAMMA-a*R1/(R1-R0)))/v == 0]; % dR1/dt = 0
     vars = [R1];
     AnswR1 = solve(eqns, vars);
 elseif V(1) <= 0
