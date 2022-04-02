cd52b677666340dd0a8aef95ec53aa716f91d29c
bsbbs <boshen89@gmail.com>
2022-04-02 18:30:46 +0800

    Froemke

--- Froemke/Extrnl_Noise/Extrnl_Noise.m
+++ Froemke/Extrnl_Noise/Extrnl_Noise.m
@@ -1,8 +1,10 @@
 %% define paths
-Homedir = 'C:\Users\Bo';
-% Homedir = '~';
+% Homedir = 'C:\Users\Bo';
+Homedir = '~';
 addpath(genpath(fullfile(Homedir,'Documents','LDDM','Froemke')));
-out_dir = 'G:\My Drive\LDDM\Froemke\Extrnl_Noise';
+% Glgdir = 'G:\My Drive';
+Glgdir = '/Volumes/GoogleDrive/My Drive';
+out_dir = fullfile(Glgdir, 'LDDM/Froemke/Extrnl_Noise');
 if ~exist("out_dir",'dir')
     mkdir(out_dir);
 end
@@ -19,6 +21,11 @@ end
 fontsize = 14;
 mksz = 5;
 lwd = 1.5;
+cp = [.032, .064,.128, .256];
+mygray = flip(gray(numel(cp) + 2));
+myred = mygray; myred(:,1) = 1;
+myblue = mygray; myblue(:,3) = 1;
+c = .064;
 %% parameters for simulation
 dt = .001; % second
 tauR = .1; % second
@@ -27,49 +34,51 @@ tauI = .1; % second
 Tau = [tauR, tauG, tauI];
 w0 = 1;
 a0 = 15;
-b0 = 1.8;
+b0 = 1.1;
 predur = 0;
 presentt = dt;
 thresh = 70; % Hz
 stoprule = 1;
 eqlb = 32;
-scale = 2*w0*eqlb.^2 + (1-a0).*eqlb;
+scale = (2*w0 - b0)*eqlb^2 + (1-a0)*eqlb; %2*w0*eqlb.^2 + (1-a0).*eqlb;
 Vprior = [1,1]*scale;
-%% General property 1. representation dynamic
+%% Representation dynamic
 task = 'VR_RT';
+a = a0*eye(2);
+b = b0*eye(2);
+w = ones(2);
 stimdur = Inf;
 triggert = Inf;
 dur = 1.5; % second
-c = [.064]'; % Coherence
-c1 = [1 + c];
-c2 = [1 - c];
 % Dynamics 
 sgm = .01;
-% sgmInput = 150; for OU process
-sgmInput = 0%1/3;
-Vinput = [c1, c2]*scale;
+sgmInput = 1/3;
+Vinput = [1 + c, 1 - c]*scale;
 a = a0*eye(2);
 b = b0*eye(2);
 w = w0*ones(2);
-iSTDP = 1;
-% initialvals = ones(3,2)*10; %(a-1)./sum(w,2)'.*[1,1; sum(w,2)'; 0, 0];
-initialvals = (a0-1)./(2*w0).*[1, 1; w0*2, w0*2; 0, 0]/.5;
-initialvals = [1,1;2-b0,2-b0;b0,b0]*eqlb;
+initialvals = [1,1;2,2;0,0]*eqlb/3;
 h = figure;
 filename = sprintf('timeCourse_%s_sgm%2.2f_sgmInput%.2f',task, sgm, sgmInput);
 % Value representation task
 rng(5);
-[choice, rt, R, G, I, Vcourse] = LDDM_STDP_RndInput(Vprior, Vinput, iSTDP, w, a, b,...
+[choice, rt, R, G, I, Vcourse] = LDDM_RndInput(Vprior, Vinput, w, a, b,...
     sgm, sgmInput*scale, Tau, predur, dur, dt, presentt, triggert, thresh, initialvals, stimdur, stoprule);
 subplot(2,1,1); hold on;
 x = (1:numel(R(:,1)))*dt;
 plot(x, R(:,1), 'r-', 'LineWidth', lwd);
 plot(x, R(:,2), 'b-', 'LineWidth', lwd);
-xlim([-50,dur/dt]);
-ylim([-5,thresh]);
+xlim([-50*dt, dur]);
+% plot(x, G(:,1), 'r--', 'LineWidth', lwd);
+% plot(x, G(:,2), 'b--', 'LineWidth', lwd);
+% plot(x, I(:,1), 'r-.', 'LineWidth', lwd);
+% plot(x, I(:,2), 'b-.', 'LineWidth', lwd);
+yticks([10, 15, 20, 25]);
 ylabel('Firing rates (Hz)');
-xlabel('Time (s)');
-legend({'R_1','R_2'},'Box','off');
+xlabel(' ');
+legend({'R_1','R_2'},'Box','off','Location','southeast',...
+    'FontName','Times New Roman', 'FontAngle','italic',...
+    'FontSize',fontsize-4);
 savefigs(h, filename, plotdir, fontsize, [2.9,4]);
 
 % Reaction-time task
@@ -77,20 +86,128 @@ stimdur = Inf;
 triggert = presentt;
 dur = 1.5; % second
 rng(8);
-[choice, rt, R, G, I, Vcourse] = LDDM_STDP_RndInput(Vprior, Vinput, iSTDP, w, a, b,...
-    sgm, sgmInput*scale, Tau, predur, dur, dt, presentt, triggert, thresh, initialvals, stimdur, stoprule);
+R0 = ((a0-1)+sqrt((1-a0)^2 + 4*scale*(2*w0 - b0)))/2/(2*w0 - b0);
+I0 = b0*R0;
+G0 = (2*w0-b0)*R0;
+initialvals = [R0,R0; G0,G0; I0,I0];
 subplot(2,1,2); hold on;
-x = (1:numel(R(:,1)))*dt;
-plot(x, R(:,1), 'r-', 'LineWidth', lwd);
-plot(x, R(:,2), 'b-', 'LineWidth', lwd);
-%xlim([-50,dur/dt]);
-ylim([-5,thresh]);
+rtvec = [];
+for vi = 1:4
+    Vinput = [1+cp(vi), 1-cp(vi)]*scale;
+    [choice, rt, R, G, I, Vcourse] = LDDM_RndInput(Vprior, Vinput, w, a, b,...
+        sgm, sgmInput*scale, Tau, predur, dur, dt, presentt, triggert, thresh, initialvals, stimdur, stoprule);
+    rtvec(vi) = rt;
+    x = (1:numel(R(:,1)))*dt;
+    plot(x, R(:,1), '-', 'LineWidth', lwd, 'Color', myred(vi+1,:));
+    plot(x, R(:,2), '-', 'LineWidth', lwd, 'Color', myblue(vi+1,:));
+    % plot(x, G(:,1), 'r--', 'LineWidth', lwd);
+    % plot(x, G(:,2), 'b--', 'LineWidth', lwd);
+    % plot(x, I(:,1), 'r-.', 'LineWidth', lwd);
+    % plot(x, I(:,2), 'b-.', 'LineWidth', lwd);
+end
+plot(rtvec, thresh*ones(size(rtvec)),'k-','LineWidth',.5);
+text(mean(rtvec), thresh*1.05,'Threshold','FontName','Arial');
+xlim([-50*dt, dur]);
+ylim([10,thresh]);
 ylabel('Firing rates (Hz)');
 xlabel('Time (s)');
 savefigs(h, filename, plotdir, fontsize, [2.9,4]);
+%% General property 1. integration of noise
+predur = 0;
+dur = 100;
+triggert = Inf;
+stimdur = Inf;
+presentt = dt;
+Vinput = [1+c, 1-c]*scale;
+rng(4);
+[choice, rt, R, G, I, Vcourse] = LDDM_RndInput(Vprior, Vinput, w, a, b,...
+    sgm, sgmInput*scale, Tau, predur, dur, dt, presentt, triggert, thresh, initialvals, stimdur, stoprule);
+%%
+h = figure; 
+filename = 'Input-output_baseline';
+subplot(1,2,1); hold on;
+h1 = histogram(Vcourse(2000:end,1),100);
+h1.FaceColor = myred(3,:);
+h1.EdgeColor = 'n';
+h2 = histogram(Vcourse(2000:end,2),100);
+h2.FaceColor = myblue(3,:);
+h2.EdgeColor = 'n';
+xlabel('Input value (a.u.)');
+ylabel(' ');
+yticks([]);
+savefigs(h, filename, plotdir, fontsize, [4, 2]);
+
+subplot(1,2,2); hold on;
+h3 = histogram(R(2000:end,1),100);
+h3.FaceColor = myred(3,:);
+h3.EdgeColor = 'n';
+h4 = histogram(R(2000:end,2),100);
+h4.FaceColor = myblue(3,:);
+h4.EdgeColor = 'n';
+xlabel('Firing rates (Hz)');
+ylabel(' ');
+yticks([]);
+savefigs(h, filename, plotdir, fontsize, [4, 2]);
+%% Vcourse
+h = figure; hold on
+filename = 'Vcourse';
+x = (1:numel(Vcourse(1:5000,1)))*dt;
+plot(x,Vcourse(1:5000,1), 'r-');
+plot(x,Vcourse(1:5000,2), 'b-');
+xlim([0, dur/20]);
+xlabel('Time (s)');
+ylabel('Input value (a.u.)');
+savefigs(h, filename, plotdir, fontsize, [3.2, 2]);
 %% General property 2. predicted choice behavior
+task = 'RT';
+predur = 0;
+presentt = dt;
+triggert = presentt;
+stimdur = Inf;
+dur = 4.5;
+sims = 102400;
+sgm = .01;
+sgmInput = 1/3;
+w = w0*ones(2);
+a = a0*eye(2);
+b = b0*eye(2);
+thresh = 70;
+stoprule = 1;
+initialvals = [R0,R0; G0,G0; I0,I0];
+filename = sprintf('LDDM_%s_a%1.2f_b%1.2f_sgm%1.1fsinpt%0.3f_sims%i',task,a0,b0,sgm,sgmInput,sims);
+output = fullfile(Simdir,[filename, '.mat']);
+cp = [2 4 8 16 32 64 128 256 512]'/1000;
+ACC = [];
+meanRT = [];
+clear Vinput Vprior;
+if ~exist(output,'file')
+    V1 = scale*(1+cp);
+    V2 = scale*(1-cp);
+    Vinput.V1 = V1; %scale*[1+cp, 1-cp];%*boost(level);
+    Vinput.V2 = V2;
+    Vprior.V1 = ones(size(V1))*scale;
+    Vprior.V2 = ones(size(V2))*scale;
+    [rt, choice, ~] = LDDM_Rndinput_GPU(Vprior, Vinput, w, a, b,...
+        sgm, sgmInput*scale, Tau, predur, dur, dt, presentt, triggert, thresh, initialvals, stimdur, stoprule, sims);
+    ACC = gather(mean(2-squeeze(choice),2,'omitnan'));
+    meanRT = gather(mean(squeeze(rt),2,'omitnan'));
+    save(output,'ACC','meanRT');
+else
+    load(output);
+end
+h = figure;
+filename = 'RT_ACC_baseline';
+subplot(2,1,1); hold on;
+plot(cp,ACC,'k.-');
+set(gca,'XScale','log');
+ylabel('% correct');
+savefigs(h, filename, plotdir, fontsize, [2.9, 4]);
 
-
+subplot(2,1,2); hold on;
+plot(cp,meanRT,'k.-');
+set(gca,'XScale','log');
+ylabel('RT (s)');
+savefigs(h, filename, plotdir, fontsize, [2.9, 4]);
 %% E-E only, self-excitation
  
 %% E-E only, input
--- Froemke/utils/savefigs.m
+++ Froemke/utils/savefigs.m
@@ -11,11 +11,6 @@ yl = get(gca,'YLabel');
 yAX = get(gca,'YAxis');
 set(yAX,'FontSize', fontsize-2)
 set(yl, 'FontSize', fontsize);
-axis tight;
-% 
-% xh = get(gca, 'xlabel'); p = get(xh, 'position'); p(2) = p(2)+abs(p(2))*.9; set(xh, 'position',p);
-% yh = get(gca, 'ylabel'); p = get(yh, 'position'); p(1) = p(1)+abs(p(1))*.8; set(yh, 'position',p);
-
 h.PaperUnits = 'inches';
 h.PaperPosition = [0 0 aspect];
 saveas(h,fullfile(outdir,sprintf('%s.eps',filename)),'epsc2');
