81223003559812885af39359574f68d2e9558d85
bsbbs <boshen89@gmail.com>
2022-12-09 15:20:56 -0500

    Fit LCA

---
+++ Fit/SvrCode/LCAFitBhvr5Params_QMLE_GPU.m
@@ -0,0 +1,68 @@
+function [nLL, Chi2, BIC, AIC, rtmat, choicemat] = LCAFitBhvr5Params_QMLE_GPU(params, dataBhvr)
+% reload Roitman's data, processed
+q = dataBhvr.q;
+On = dataBhvr.On;
+ON = dataBhvr.ON;
+OP = dataBhvr.OP;
+% parameters to fit
+k = params(1)*eye(2);
+beta = params(2)*eye(2);
+sgm = params(3);
+T0 = params(4);
+thresh = params(5);
+
+% other fixed parameters
+% sims = 1024;
+deduction = .1;
+sims = 1024/deduction;
+Cohr = [0 32 64 128 256 512]/1000; % percent of coherence
+dur = 5;
+dt =.001;
+
+V1 = (1 + Cohr)';
+V2 = (1 - Cohr)';
+RhoMat = [V1, V2];
+
+% tic
+[rtmat, choicemat, ~] = LCA_GPU(RhoMat, k, beta, sgm, T0, thresh, dur, dt, sims);
+rtmat = squeeze(rtmat)';
+choicemat = squeeze(choicemat)';
+% toc;
+
+%% for reaction time by histogram
+% QMLE, quantile maximum likelihood estimation
+% reference: Heathcote & Australia, and Mewhort, 2002.
+% Chi-square, reference: Ratcliff & McKoon, 2007.
+LL = log(0);
+Chi2 = 0;
+h = figure;
+for vi = 1:6
+    En(vi) = numel(rtmat(:,vi));
+    RT_corr = rtmat(choicemat(:,vi) == 1,vi);
+    RT_wro = rtmat(choicemat(:,vi) == 2,vi);
+    if ~all(isnan(q(:,1,vi)))
+        tmp = histogram(RT_corr, [0; q(:,1,vi); Inf], 'Visible',0);
+        EN(:,1,vi) = tmp.Values;
+    else
+        EN(:,1,vi) = NaN(numel(q(:,1,vi))+1,1);
+    end
+    if ~all(isnan(q(:,2,vi)))
+        tmp = histogram(RT_wro, [0; q(:,2,vi); Inf], 'Visible',0);
+        EN(:,2,vi) = tmp.Values;
+    else
+        EN(:,2,vi) =  NaN(numel(q(:,2,vi))+1,1);
+    end
+    f(:,:,vi) = log((EN(:,:,vi)/En(vi)));
+    f(f(:,1,vi) == -Inf,1,vi) = log(.1/En(vi)); % set floor value of f at each point, to prevent -Inf
+    f(f(:,2,vi) == -Inf,2,vi) = log(.1/En(vi)); % set floor value of f at each point, to prevent -Inf
+    ON_adj(:,:,vi) = ON(:,:,vi)*En(vi)./On(vi);
+end
+close(h);
+LL = sum(ON(:).*f(:),'omitnan');
+Chi2vec = (EN - ON_adj).^2./EN;
+Chi2 = sum(Chi2vec(:),'omitnan');
+n = sum(~isnan(ON(:).*f(:)));
+k = numel(params);
+BIC = k*log(n) - 2*LL;
+AIC = 2*k - 2*LL;
+nLL = -LL;
---
+++ Fit/main_FitLCA.m
@@ -0,0 +1,113 @@
+
+%% setup directory
+addpath('../../RecurrentModel');
+numNode = 1;
+[sortNum, myCluster] = RndCtrl(numNode);
+mypool = parpool(myCluster, myCluster.NumWorkers);
+
+%% Model fitting with Bayesian Adaptive Direct Search (BADS) optimization algorithm
+addpath(genpath('../../RecurrentModel/bads/bads-master'));
+addpath('../CoreFunctions/');
+addpath('./SvrCode/');
+out_dir = '../../RecurrentModel/Fit/Rslts/LCAFitBhvr5Params_QMLE_SvrGPU';
+if ~exist(out_dir,'dir')
+    mkdir(out_dir);
+end
+%%
+% Take data from Roitman & Shadlen, 2002
+dataDynmc = load('../../RecurrentModel/Fit/Data/Data.mat');
+dataBhvr = LoadRoitmanData('../../RecurrentModel/RoitmanDataCode');
+% Fix random seed for reproducibility
+% rng(1);
+% change random seed
+t = datenum(clock)*10^10 - floor(datenum(clock)*100)*10^8 + sortNum*10^7;
+num2str(t);
+rng(t);
+
+% Define optimization starting point and bounds
+%     k,    beta, noise, T0, thresh
+LB = [0    0.1   .1    .1*256 [.001,.001,.001]];
+UB = [70   3	100  20*256 [1,1,1]];
+PLB = [15  .9	5    1*256 [.01 .01 .01]];
+PUB = [60   1.7	40   8*256 [.2 .2 .2]];
+
+% Randomize initial starting point inside plausible box
+x0 = rand(1,numel(LB)) .* (PUB - PLB) + PLB;
+
+% likelihood function
+% parpool(6);
+nLLfun = @(params) LCAFitBhvr5Params_QMLE_GPU(params, dataBhvr)
+[fvalbest,~,~] = nLLfun(x0)
+fprintf('test succeeded\n');
+
+% loop begin
+Collect = [];
+parfor i = 1:myCluster.NumWorkers*8
+    !ping -c 1 www.amazon.com
+    t = datenum(clock)*10^10 - floor(datenum(clock)*100)*10^8 + sortNum*10^7 + i*10^5;
+    %num2str(t);
+    rng(t);
+    
+    % Randomize initial starting point inside plausible box
+    x0 = rand(1,numel(LB)) .* (PUB - PLB) + PLB;
+    dlmwrite(fullfile(out_dir,'x0List.txt'),[sortNum, i, t, x0],'delimiter','\t','precision','%.6f','-append');
+    % fit
+    options = bads('defaults');     % Default options
+    options.Display = 'iter';
+    % options.UncertaintyHandling = false;    % Function is deterministic
+    options.UncertaintyHandling = true;    % Function is deterministic
+    [xest,fval,~,output] = bads(nLLfun,x0,LB,UB,PLB,PUB,options);
+    %     xest
+    %     fval
+    %     output
+    dlmwrite(fullfile(out_dir,'RsltList.txt'),[sortNum, i, t, xest fval],'delimiter','\t','precision','%.6f','-append');
+    %     if fval < fvalbest
+    %         xbest = xest;
+    %         fvalbest = fval;
+    %         outputbest = output;
+    %     end
+    % save(fullfile(out_dir,sprintf('./Rslts%i_%i.mat', sortNum, i)),'xest','fval','output');
+    Collect(i).rndseed = t;
+    Collect(i).x0 = x0;
+    Collect(i).xest = xest;
+    Collect(i).fval = fval;
+    Collect(i).output = output;
+    
+end
+t = datenum(clock)*10^10 - floor(datenum(clock)*100)*10^8 + sortNum*10^7 + i*10^5;
+save(fullfile(out_dir,sprintf('CollectRslts%i.mat',t)),'Collect');
+
+
+%% visulization
+if 0
+    addpath(genpath('/Users/bs3667/Documents/LDDM/utils'));
+    addpath(genpath('/Users/bs3667/Documents/LDDM/CoreFunctions'));
+    lwd = 1.5;
+    fontsize = 14;
+    aspect = [4, 3];
+    outdir = '/Volumes/GoogleDrive/My Drive/LDDM/Fit/Rslts/FitLCA';
+    %% The dynamic of single trials
+    mygray = gray(8);
+    h = figure; hold on;
+    filename = 'LCA_Dynamic_sngltrials';
+    i = 0;
+    for c = [0, .032, .064, .128, .356, .512]
+        i = i + 1;
+        rho = [1+c, 1-c];
+        k = .1*eye(2);
+        beta = (ones(2) - eye(2))*.6;
+        sgm = .2;
+        T0 = .1;
+        thresh = 1;
+        dur = 5;
+        dt = .001;
+        [choice, rt, x] = LCA(rho, k, beta, sgm, T0, thresh, dur, dt);
+        t = (1:length(x(:,1)))*dt;
+        plot(t, x(:,1), '-', 'Color' , mygray(i,:), 'LineWidth', lwd);
+        plot(t, x(:,2), '--', 'Color' , mygray(i,:), 'LineWidth', lwd);
+        
+    end
+    ylabel('Neural Activity (a.u.)');
+    xlabel('Time (Secs)');
+    savefigs(h, filename, outdir, fontsize, aspect);
+end
