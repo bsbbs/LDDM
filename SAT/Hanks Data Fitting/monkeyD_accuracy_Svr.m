5c5a18d740411bae779a2cae7e38251afee4d230
Shen <bs3667@bigpurple-ln2.cm.cluster>
2022-12-20 14:47:00 -0500

    Fit Hanks

--- SAT/Hanks Data Fitting/monkeyD_accuracy_Svr.m
+++ SAT/Hanks Data Fitting/monkeyD_accuracy_Svr.m
@@ -2,16 +2,15 @@
 % write your own help language here
 
 %% setup directory
-addpath(genpath('./SAT'));
-addpath('./utils');
+addpath('../../utils');
 numNode = 1;
 [sortNum, myCluster] = RndCtrl(numNode);
 mypool = parpool(myCluster, myCluster.NumWorkers);
 
 
 %% Model fitting with Bayesian Adaptive Direct Search (BADS) optimization algorithm
-addpath(genpath('../RecurrentModel/bads/bads-master'));
-out_dir = '../LDDM_Output/SAT/Hanks/monkeyD_accuracy_Svr';
+addpath(genpath('../../../RecurrentModel/bads/bads-master'));
+out_dir = '../../../LDDM_Output/SAT/Hanks/monkeyD_accuracy_Svr';
 if ~exist(out_dir,'dir')
     mkdir(out_dir);
 end
