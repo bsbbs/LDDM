60414a01bdd30b951451401e09579ca88489c7d5
bsbbs <boshen89@gmail.com>
2022-12-21 16:48:34 -0500

    LL space

--- Fit/LLSpace_LDDM7IV.m
+++ Fit/LLSpace_LDDM7IV.m
@@ -10,10 +10,7 @@ if ~exist(out_dir,'dir')
     mkdir(out_dir);
 end
 
-%% Visuliazation parameters
-lwd = 2.0;
-mksz = 18;
-fontsize = 14;
+
 %% load empirical data
 dataBhvr = LoadRoitmanData(fullfile(Glgdir,'RoitmanDataCode'));
 
@@ -22,21 +19,39 @@ dataBhvr = LoadRoitmanData(fullfile(Glgdir,'RoitmanDataCode'));
 bestparams = [0	1.433631	25.35945	3251.289056	0.185325	0.224459	0.323132 16539.138186];
 names = {'alpha','beta','sgm','S','tauR','tauG','tauD'};
 %% Check the space of alpha and beta
-ivec = 10.^[-1:.01:3];
-jvec = linspace(0,4,401);
+ivec = linspace(0,100,41); % [0, 10.^[-1:.1:3]]; %10.^[-1:.01:3];
+jvec = linspace(0,4,41); %linspace(0,4,401);
 idx = [1, 2];
 [filename, nLLmat] = SpaceCheck(bestparams, names, ivec, jvec, idx, dataBhvr, out_dir);
-Visualization(out_dir,filename);
-%% Check the space of scale and noise
-
+Visualization(out_dir,filename, names, idx);
+h = figure;
+contour(nLLmat);
+%% Check the space of noise and scale
+ivec = linspace(0,80,41); % noise, best fit = 25.36 
+jvec = linspace(10, 8010, 41); % 10.^[-1:.1:3]*9; % scale, best fit = 3251
+idx = [3, 4];
+[filename, nLLmat] = SpaceCheck(bestparams, names, ivec, jvec, idx, dataBhvr, out_dir);
+Visualization(out_dir,filename, names, idx);
 %% Check the space of tauR and tauG
-
-%% Check the space of tauR and tauD
-
+ivec = 10.^[-2:.1:1]; %linspace(.025,1.05,42); % tauR, best fit = .18
+jvec = 10.^[-2:.1:1]; %linspace(.025,1.025,41); % tauG, best fit = .22
+idx = [5, 6];
+[filename, nLLmat] = SpaceCheck(bestparams, names, ivec, jvec, idx, dataBhvr, out_dir);
+Visualization(out_dir,filename, names, idx);
+% Check the space of tauR and tauD
+ivec = 10.^[-2:.1:1]; %linspace(.025,1.05,42); % tauR, best fit = .18
+jvec = 10.^[-2:.1:1]; %linspace(.025,1.025,41);% tauD, best fit = .32
+idx = [5, 7];
+[filename, nLLmat] = SpaceCheck(bestparams, names, ivec, jvec, idx, dataBhvr, out_dir);
+Visualization(out_dir,filename, names, idx);
 %% Check the space of tauG and tauD
+ivec = 10.^[-2:.1:1]; %linspace(.025,1.05,42); % tauG, best fit = .22
+jvec = 10.^[-2:.1:1]; %linspace(.025,1.025,41); % tauD, best fit = .32
+idx = [6, 7];
+[filename, nLLmat] = SpaceCheck(bestparams, names, ivec, jvec, idx, dataBhvr, out_dir);
+Visualization(out_dir,filename, names, idx);
+%% functions
 function [filename, nLLmat] = SpaceCheck(bestparams, names, ivec, jvec, idx, dataBhvr, out_dir)
-maxNcores = 6;
-mypool = parpool(maxNcores);
 iN = length(ivec);
 jN = length(jvec);
 filename = sprintf('LLSpace_%s%i_%s%i', names{idx(1)}, iN, names{idx(2)}, jN);
@@ -44,7 +59,7 @@ if ~exist(fullfile(out_dir,[filename, '.mat']), 'file')
     nLLmat = NaN(iN, jN);
     for i = 1:iN
         ival = ivec(i);
-        fprintf('%2.2f %%\n', i/iN);
+        fprintf('%02.1f %%\n', i/iN*100);
         parfor j = 1:jN
             params = bestparams;
             params(idx) = [ival, jvec(j)];
@@ -58,10 +73,17 @@ else
 end
 end
 
-function Visualization(out_dir,filename)
+function Visualization(out_dir,filename, names, idx)
+% Visuliazation parameters
+lwd = 2.0;
+mksz = 18;
+fontsize = 14;
 load(fullfile(out_dir,[filename, '.mat']));
-h = figure;
+h = figure; hold on;
 s = surf(nLLmat,'EdgeColor','none');
+minnLL = min(nLLmat(:));
+[r, c] = find(nLLmat == minnLL);
+plot3(c,r,minnLL, 'rx', 'MarkerSize', mksz, 'LineWidth', lwd);
 ylim([1,length(ivec)]);
 xlim([1,length(jvec)]);
 xticks(linspace(1,length(jvec),5));
@@ -70,6 +92,9 @@ yticks(linspace(1,length(ivec),5));
 yticklabels(ivec(linspace(1,length(ivec),5))); % {'10^{-1}','10^0','10^1','10^2','10^3'});
 xlabel(names{idx(2)});
 ylabel(names{idx(1)});
+clb = colorbar;
+ylabel(clb, 'Negative Log likelihood');
 view(0,90);
+grid on;
 savefigs(h, filename, out_dir, fontsize - 2, [2.8 2.54]*.95);
 end
