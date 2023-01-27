8cdd0bc68532720e04a5b5f83baa0b92f701054a
bsbbs <boshen89@gmail.com>
2023-01-27 10:52:06 -0500

    PrmtreRcvry LDDM

--- Fit/PrmtrsRcvry_LDDM7IV.m
+++ Fit/PrmtrsRcvry_LDDM7IV.m
@@ -1,10 +1,12 @@
 %% set path
-Homedir = '~/Documents';
+Homedir = 'C:\Users\Bo\Documents';
+% Homedir = '~/Documents';
 addpath(fullfile(Homedir, 'LDDM','CoreFunctions'));
 addpath(fullfile(Homedir, 'LDDM','utils'));
 addpath(fullfile(Homedir, 'LDDM', 'Fit','SvrCode'));
 
-Glgdir = '/Volumes/GoogleDrive/My Drive/LDDM';
+Glgdir = 'G:\My Drive\LDDM';
+% Glgdir = '/Volumes/GoogleDrive/My Drive/LDDM';
 % addpath(genpath(fullfile(Glgdir, 'Fit','bads-master')));
 addpath(genpath(fullfile(Glgdir, 'Fit','bads')));
 out_dir = fullfile(Glgdir,'Fit/Rslts/FitBhvr7ParamsIV_QMLE_SvrGPU/PrmtrsRcvry');
@@ -17,7 +19,7 @@ end
 % a, b, noise, scale, tauR, tauG, tauD, nLL
 bestparams = [0	1.433631	25.35945	3251.289056	0.185325	0.224459	0.323132 16539.138186];
 names = {'alpha','beta','sgm','S','tauR','tauG','tauD'};
-
+latexnames = {'\alpha','\beta','\sigma','S','\tau_R','\tau_G','\tau_D'};
 %% generating 'fake' data
 % load empirical data - not really needed
 dataBhvr = LoadRoitmanData(fullfile(Glgdir,'RoitmanDataCode'));
@@ -26,6 +28,84 @@ dataBhvr = LoadRoitmanData(fullfile(Glgdir,'RoitmanDataCode'));
 
 %% calculate indicators
 SimDataQp = Load_SimData(rtmat, choicemat);
+%% Check the space of alpha and beta
+ivec = linspace(0,100,41); % [0, 10.^[-1:.1:3]]; %10.^[-1:.01:3];
+jvec = linspace(0,4,41); %linspace(0,4,401);
+idx = [1, 2];
+[filename, nLLmat] = SpaceCheck(bestparams, names, ivec, jvec, idx, SimDataQp, out_dir);
+Visualization(out_dir,filename, names, idx, 5, latexnames);
+%% Check the space of noise and scale
+ivec = linspace(0,80,41); % noise, best fit = 25.36 
+jvec = linspace(10, 8010, 41); % 10.^[-1:.1:3]*9; % scale, best fit = 3251
+idx = [3, 4];
+[filename, nLLmat] = SpaceCheck(bestparams, names, ivec, jvec, idx, SimDataQp, out_dir);
+Visualization(out_dir,filename, names, idx, 5, latexnames);
+%% Check the space of tauR and tauG
+ivec = 10.^[-2:.1:1]; %linspace(.025,1.05,42); % tauR, best fit = .18
+jvec = 10.^[-2:.1:1]; %linspace(.025,1.025,41); % tauG, best fit = .22
+idx = [5, 6];
+[filename, nLLmat] = SpaceCheck(bestparams, names, ivec, jvec, idx, SimDataQp, out_dir);
+Visualization(out_dir,filename, names, idx, 4, latexnames);
+%% Check the space of tauR and tauD
+ivec = 10.^[-2:.1:1]; %linspace(.025,1.05,42); % tauR, best fit = .18
+jvec = 10.^[-2:.1:1]; %linspace(.025,1.025,41);% tauD, best fit = .32
+idx = [5, 7];
+[filename, nLLmat] = SpaceCheck(bestparams, names, ivec, jvec, idx, SimDataQp, out_dir);
+Visualization(out_dir,filename, names, idx, 4, latexnames);
+%% Check the space of tauG and tauD
+ivec = 10.^[-2:.1:1]; %linspace(.025,1.05,42); % tauG, best fit = .22
+jvec = 10.^[-2:.1:1]; %linspace(.025,1.025,41); % tauD, best fit = .32
+idx = [6, 7];
+[filename, nLLmat] = SpaceCheck(bestparams, names, ivec, jvec, idx, SimDataQp, out_dir);
+Visualization(out_dir,filename, names, idx, 4, latexnames);
+%% functions
+function [filename, nLLmat] = SpaceCheck(bestparams, names, ivec, jvec, idx, dataBhvr, out_dir)
+iN = length(ivec);
+jN = length(jvec);
+filename = sprintf('LLSpace_%s%i_%s%i', names{idx(1)}, iN, names{idx(2)}, jN);
+if ~exist(fullfile(out_dir,[filename, '.mat']), 'file')
+    nLLmat = NaN(iN, jN);
+    for i = 1:iN
+        ival = ivec(i);
+        fprintf('%02.1f %%\n', i/iN*100);
+        parfor j = 1:jN
+            params = bestparams;
+            params(idx) = [ival, jvec(j)];
+            [nLL, ~,~,~,~,~] = LDDMFitBhvr7ParamsIV_QMLE_GPU(params, dataBhvr);
+            nLLmat(i,j) = nLL;
+        end
+    end
+    save(fullfile(out_dir,[filename, '.mat']), 'nLLmat', 'ivec','jvec','idx');
+else
+    load(fullfile(out_dir,[filename, '.mat']));
+end
+end
+
+function Visualization(out_dir,filename, names, idx, nticks, latexnames)
+% Visuliazation parameters
+lwd = 2.0;
+mksz = 18;
+fontsize = 14;
+load(fullfile(out_dir,[filename, '.mat']));
+h = figure;
+hold on;
+minnLL = min(nLLmat(:));
+[r, c] = find(nLLmat == minnLL);
+contour(-nLLmat,60);
+plot3(c,r,-minnLL*1.01, 'rx', 'MarkerSize', mksz/3, 'LineWidth', lwd);
+xticks(linspace(1,length(jvec),nticks));
+xticklabels(jvec(linspace(1,length(jvec),nticks)));
+yticks(linspace(1,length(ivec),nticks));
+yticklabels(ivec(linspace(1,length(ivec),nticks)));
+xlabel(latexnames{idx(2)},'FontAngle', 'italic');
+ylabel(latexnames{idx(1)},'FontAngle', 'italic');
+clb = colorbar;
+colormap('turbo');
+ylabel(clb, 'Log likelihood');
+savefig(h, fullfile(out_dir,['Contour',filename]));
+savefigs(h, ['Contour',filename], out_dir, fontsize - 2, [4 3]);
+% close(h);
+end
 
 %% refit
 % Define optimization starting point and bounds
