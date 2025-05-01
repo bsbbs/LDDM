5179e59ef1e2d7650946694cb266b28f619851db
Bo Shen <40451542+bsbbs@users.noreply.github.com>
2025-04-30 20:05:35 -0400

    STDP

--- Froemke/Revision1/Demo.m
+++ Froemke/Revision1/Demo.m
@@ -6,7 +6,7 @@ Homedir = 'C:\Users\Bo\Documents\GitHub';
 % addpath(fullfile(Homedir,'Documents','LDDM','CoreFunctions'));
 addpath(fullfile(Homedir,'LDDM','Froemke','Revision1'));
 addpath(fullfile(Homedir,'LDDM','Froemke','utils'));
-Drpbx = '/Users/bs3667/NYU Langone Health Dropbox/Shen Bo/Bo Shen Working files/STDP_Project0';
+Drpbx = "C:\Users\Bo\NYU Langone Health Dropbox\Shen Bo\Bo Shen Working files\STDP_Project0";
 out_dir = fullfile(Drpbx, 'Revision1');
 if ~exist("out_dir",'dir')
     mkdir(out_dir);
@@ -34,8 +34,8 @@ tauG = .1; % second
 tauD = .1; % second
 Tau = [tauR, tauG, tauD];
 w0 = 1;
-a0 = 15;
-b0 = 1; % 1.1;
+a0 = 0;
+b0 = 1.1; % 1.1;
 c_rprsnt = .064;
 sgmInput_rprsnt = 1/3;
 c_choice = .032;
@@ -45,8 +45,7 @@ presentt = dt;
 thresh = 70; % Hz
 stoprule = 1;
 eqlb = 32;
-scale = (2*w0 - b0)*eqlb^2 + (1-a0)*eqlb; %2*w0*eqlb.^2 + (1-a0).*eqlb;
-R0 = ((a0-1)+sqrt((1-a0)^2 + 4*scale*(2*w0 - b0)))/2/(2*w0 - b0);
-D0 = b0*R0;
-G0 = (2*w0-b0)*R0;
-Vprior = [1,1]*scale;
+BG = 30;
+BR = 30;
+
+%%
